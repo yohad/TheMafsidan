56b4faa0935f03eeb0bf6b4f7a8de1e3a30de334
Yotam Ohad <yotam2206@gmail.com>
2018-03-06 11:27:56 +0200

    styling is finished

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,3 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-
 module Main where
 
 import Yesod
@@ -28,39 +22,92 @@ mkYesod "App" [parseRoutes|
 
 instance Yesod App where
   defaultLayout = layout
-
+  
 layout :: Widget -> Handler Html
 layout widget = do
   pc <- widgetToPageContent $ do
     widget
     toWidget [cassius|
+                     html
+                       background-color: #b19cd9
+                       
                      body
                        background-color: #ffb347
                        width: 800px
                        margin-left: auto
                        margin-right: auto
+                     
                      img
-                       vertical-align: middle
+                       display: block
+                       margin-left: auto
+                       margin-right: auto
+                                        
+                     a:link
+                       color: #fa7777
+                       text-decoration: none
+                       font-weight: bold
+
+                     .footer
+                       font-style: italic
+                       font-size: 11
+
+                     ul.menu                     
+                       list-style-type: none
+                       margin: 0
+                       padding: 0
+                       overflow: hidden
+                       background-color: #333
+                       
+                     li
+                       float: left
+
+                     li a
+                       display: block
+                       color: white
+                       text-align: center
+                       padding: 14px 16px
+                       text-decoration: none
+
+                     a.head:link
+                       size: 30px
+                       color: #fa7766
+                       font-weight: bold
+
+                     a.head:visited
+                       size: 30px
+                       color: #fa7766
+                       font-weight: bold
                      |]
   withUrlRenderer
     [hamlet|
            <html>
-             <head>
-               <img src=@{StaticR logo_png}>
-               ^{pageHead pc}
-             <body>
-               <article>
-                 ^{pageBody pc}
+               <head>
+                   ^{pageHead pc}
+               <body>
+                   <img src=@{StaticR logo_png}>               
+                   <article>
+                       <ul .menu>
+                           <li>
+                             <a href=@{HomeR}>Home
+                           <li>
+                             <a href=@{PostsR}>Posts
+                       ^{pageBody pc}
+                       ^{footer}
            |]
 
-getPostsR :: Handler Html
-getPostsR = defaultLayout $ do
-  posts <- liftIO $ listDirectory postsDir
-  toWidget [whamlet|
-                   <h1>Post
-                   $forall post <- posts
-                     <a href=@{PostR post}>#{takeBaseName post}
-                   |]
+footer = do
+    [hamlet|<p .footer>This site is 100% type safe|]
+      
+mainPage = do
+    [whamlet|
+             <div .text>
+               <h1>The Mafsidan
+               <h2>The Realest of News
+               <p>This is my personal website. My goal is to make it a safe
+               haven for my weird ideas. The topics I think I'll talk about (but
+               I'm not limiting my self) are Physics, Math, CS, Games, Music and
+               Art.
+            |]
 
 getPostR :: String -> Handler Html
 getPostR name = defaultLayout $ do
@@ -72,25 +119,19 @@ getPostR name = defaultLayout $ do
                    $nothing
                      <p>Error, post does not exist.
                    |]
+  where
+    eitherToMaybe :: Either a b -> Maybe b
+    eitherToMaybe (Left _) = Nothing
+    eitherToMaybe (Right a) = Just a
 
-eitherToMaybe :: Either a b -> Maybe b
-eitherToMaybe (Left _) = Nothing
-eitherToMaybe (Right a) = Just a
-
-footer = do
-    [whamlet|<i>This site is 100% type safe|]
-    toWidget [lucius|p { font-size: 11px }|]
-      
-mainPage = do
-    [whamlet|
-             <div class="text">
-               <h1>The Mafsidan
-               <h2>The Realest of News
-               <p>This is my personal website.
-                  My goal is to make it a safe haven for my weird ideas.
-                  The topics I think I'll talk about (but I'm not limiting my self) are Physics, Math, CS, Games, Music and Art.
-               ^{footer}
-            |]
+getPostsR :: Handler Html
+getPostsR = defaultLayout $ do
+  posts <- liftIO $ listDirectory postsDir
+  toWidget [whamlet|
+                   <h1>Posts
+                   $forall post <- posts
+                     <a .head href=@{PostR post}>#{takeBaseName post}
+                   |]
       
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
